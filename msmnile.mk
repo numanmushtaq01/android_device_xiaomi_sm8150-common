d36840a152a9e7bc337ee8b93782071a2dc314b7
Michael Bestas <mkbestas@lineageos.org>
2023-03-03 23:43:42 +0200

    sm8150-common: Build Lineage Health HAL
     -also enable charging control
    
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -245,6 +245,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
